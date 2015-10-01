8958302620cef6cf1288c6868134e549b66cde7c
DmitryKrohmal <Dmitrii_Krohmal@mail.ru>
2015-10-01 08:55:39 +0300

    added fix for appveyor.yml + fix version script

--- VersionSetterScript.ps1
+++ VersionSetterScript.ps1
@@ -11,5 +11,14 @@ $versionOld = "{0}.{1}.{2}" -f $matchVar1["major"],$matchVar1["minor"],$matchVar
 
 
 (Get-Content MindKeeper\Installer\MKInstaller.iss) | 
-Foreach-Object {$_ -replace $versionOld,$versionNew}  | 
+Foreach-Object {$_ -replace $versionOld,$versionNew}  | ;
 Out-File -Encoding UTF8 MindKeeper\Installer\MKInstaller.iss
+
+$contentCI = Get-Content MindKeeper\Installer\MKInstaller.iss
+"'"+$contentCI+"'" -match 'release: MK-v\((?<major>\d+).(?<minor>\d+).(?<patch>\d+)\)"'
+$matchVar2 = $matches
+$versionOldCI = "{0}.{1}.{2}" -f $matchVar2["major"],$matchVar2["minor"],$matchVar2["patch"]
+
+(Get-Content appvyor.yml) | 
+Foreach-Object {$_ -replace $versionOldCI,$versionNew}  | 
+Out-File -Encoding UTF8 appvyor.yml
